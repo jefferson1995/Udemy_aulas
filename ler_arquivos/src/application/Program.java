ff084cf47ed51510aef88a64ce5936d093f8cd05
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-07-17 09:41:39 -0300

    exemplo para ler arquivos

---
+++ ler_arquivos/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\jeffe\\OneDrive\\Documentos\\Jefferson Barbosa dos Santos\\Udemy\\Java completo mais projetos\\TRABALHANDO COM ARQUIVOS\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
+
