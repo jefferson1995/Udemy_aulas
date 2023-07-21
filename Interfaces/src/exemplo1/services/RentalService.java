2d91631fbd62d4637ce9c9df48081ff1a3bd1762
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-07-21 09:23:22 -0300

    services

---
+++ Interfaces/src/exemplo1/services/RentalService.java
@@ -0,0 +1,22 @@
+package exemplo1.services;
+
+import exemplo1.entities.CarRental;
+import exemplo1.entities.Invoice;
+
+public class RentalService {
+
+    private Double pricePerHour;
+    private Double pricePerDay;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+        this.pricePerHour = pricePerHour;
+        this.pricePerDay = pricePerDay;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental){
+        carRental.setInvoice(new Invoice(50.0, 10.0));
+    }
+}
