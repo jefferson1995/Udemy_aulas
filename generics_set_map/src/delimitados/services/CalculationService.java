69563314216e69de9917eaf88d869b38cb084240
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-08-03 08:47:11 -0300

    fix: CalculationService

--- generics_set_map/src/delimitados/services/CalculationService.java
+++ generics_set_map/src/delimitados/services/CalculationService.java
@@ -7,12 +7,14 @@ public class CalculationService {
     /*
         Explicação:
         Este método vai trabalhar com qualquer tipo T ->
-        Desde que o T seja qualquer subtipo de uma Compable T
+        Desde que o T seja qualquer subtipo de uma Compable T e o supert qualquer super classe dela
+
+        prática = extendends o comparação de produto ou qualquer super classe dela
 
         Nesse caso fica uma chamado generica para este método
 
      */
-    public static <T extends Comparable<T>> T max(List<T> list){
+    public static <T extends Comparable<? super T>> T max(List<T> list){
         if(list.isEmpty()){
             throw new IllegalStateException("List can't be empty");
         }
