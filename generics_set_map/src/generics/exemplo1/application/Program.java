2e524401c92088ce1ecfb85cd591aa2338cc67d5
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-08-03 08:09:16 -0300

    Generics

---
+++ generics_set_map/src/generics/exemplo1/application/Program.java
@@ -0,0 +1,34 @@
+package exemplo1.application;
+
+import exemplo1.application.exemplo1.services.PrintService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        /*
+            Onde é instanciado o tipo de classe -> Nesse caso é Integer, mas pode utilizar String tbm
+         */
+        PrintService<String> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+
+    }
+}
