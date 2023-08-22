170aacf967371c660c5adb50e87a4b214d30fbb3
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-08-22 07:57:23 -0300

    update projetoDao

--- jdbc_acesso_banco_de_dados/src/padrao_de_projeto_DAO/application/Program.java
+++ jdbc_acesso_banco_de_dados/src/padrao_de_projeto_DAO/application/Program.java
@@ -64,5 +64,13 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Novo id igual a: " + newSeller.getId());
 
+
+        //Update
+        System.out.println("\n=== TEST 5: seller Update ==== ");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update comleto");
+
     }
 }
