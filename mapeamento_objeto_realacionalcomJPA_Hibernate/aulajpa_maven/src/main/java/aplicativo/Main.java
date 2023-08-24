1bd61bc6cdfe43600806371946a4765ae92e2b8e
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-08-24 09:22:31 -0300

    Inserindo dados no banco de dados

--- mapeamento_objeto_realacionalcomJPA_Hibernate/aulajpa_maven/src/main/java/aplicativo/Main.java
+++ mapeamento_objeto_realacionalcomJPA_Hibernate/aulajpa_maven/src/main/java/aplicativo/Main.java
@@ -2,12 +2,33 @@ package aplicativo;
 
 import dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Carlos Da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(1, "Maria Da Silva", "maria@gmail.com");
-        Pessoa p3 = new Pessoa(1, "Pedro Da Silva", "pedro@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Carlos Da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Maria Da Silva", "maria@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Pedro Da Silva", "pedro@gmail.com");
+
+        //Faz as configurações de conexão
+        //Passa como parametro o nome que está dentro do arquivo persistence.xml
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+
+        EntityManager em = emf.createEntityManager(); //Cria a conexão com banco de dados
+
+
+        em.getTransaction().begin(); //Transação com banco de dados
+
+        //Salva no banco de dados
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+
+        em.getTransaction().commit(); //Para confirmar as transações realizadas
+
 
         System.out.println(p1);
         System.out.println(p2);
